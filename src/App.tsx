7bc46031828f1391337eb1b520a48be7e8d12ab5
Jayden Platell <jayden@platell.info>
2025-03-10 12:43:20 +1000

    Added Report component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import type { ReactElement } from 'react'
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const Gallery = lazy(async () => import('pages/Gallery'))
-const Details = lazy(async () => import('pages/Details'))
+// const Gallery = lazy(async () => import('pages/Gallery'))
+// const Details = lazy(async () => import('pages/Details'))
+const Report = lazy(async () => import('pages/Report'))
 
 export default function App(): ReactElement {
 	return (
 		<BrowserRouter>
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
-					<Route path='/' element={<Gallery />} />
-					<Route path=':fruitName' element={<Details />} />
+					<Route path='/' element={<Report />} />
+					{/* <Route path=':fruitName' element={<Details />} /> */}
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames'
+import type { ButtonHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+
+type ButtonProperties = ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = forwardRef<HTMLButtonElement, ButtonProperties>(
+	({ className, children, ...properties }, reference) => (
+		<button
+			type='button'
+			className={classNames(
+				'hover:bg-blue-700-hover rounded bg-blue-700 px-[22px] py-2.5 font-medium text-white sm:px-[15px] sm:py-[9px]',
+				'focus:border-focused focus:outline focus:outline-2 focus:outline-offset-1 focus:outline-blue-700',
+				className
+			)}
+			ref={reference}
+			{...properties}
+		>
+			{children}
+		</button>
+	)
+)
+Button.displayName = 'Button'
+
+export default Button
